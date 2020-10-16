b131a44243c7024dd026c4933eaf66f380dae957
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2020-10-16 19:30:45 -0400

    Update AnimatedMain.cpp

--- AnimatedMain.cpp
+++ AnimatedMain.cpp
@@ -1,6 +1,8 @@
-// made by Ariel G.
+// made by Ariel Glasroth
+// video in the tutorial is mine... 
+// this is under the appache liscene 2.0 and all that legal stuff:
 
-
+#define _CRT_SECURE_NO_WARNINGS
 
 #define OEMRESOURCE 
 #include <windows.h>
@@ -9,12 +11,23 @@
 #include <map>
 #include <vector>
 #include <utility>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libavcodec/avcodec.h>
+#include <fstream>
+
+#define INBUF_SIZE 4096
+
+
+char check = 'y';
+
 std::string fileNameS;
 std::string fileName;
 int filesToRead;
 int cursorType;
 std::pair<DWORD, std::string> value;
 int delay = 0;
+
 std::pair<DWORD,std::string> convertType(int a) { // 14 cursors
 	std::pair<DWORD,std::string> result;
 
@@ -108,16 +121,15 @@ std::pair<DWORD,std::string> convertType(int a) { // 14 cursors
 	}
 }
 
+void ImageToCursor(){
 
-int main() {
-	
 
 
 	std::cout << "NOTE; restarting your computer resets any changed cursors. \n\nto use the program (multipule can be open at once):\n1. fill a folder with the images you want to be rotated through (name example, 1.CUR)\n2. drag and drop the folder in this window\n3. click enter \n4. chose which cursor you want to animate (ex: hand arrow) - and number of images to cycle through (putting a impossible value will result in a crash)\n5. the cursor will animate\n";
 
 	std::cin >> fileNameS;
 
-	for (auto i:fileNameS) {
+	for (auto i : fileNameS) {
 		if (i != '"') {
 			fileName += i;
 		}
@@ -128,13 +140,13 @@ int main() {
 	std::cin >> filesToRead;
 
 	std::vector<HCURSOR> cursorList;
-	
+
 	for (int i = 1; i <= filesToRead; i++) {
 		std::string fileNameRevised = (fileName + "\\" + std::to_string(i) + ".CUR");
-		
+
 		cursorList.push_back(LoadCursorFromFileA(fileNameRevised.c_str()));
-		
-		if (cursorList[i-1] == NULL) {
+
+		if (cursorList[i - 1] == NULL) {
 
 			std::cout << "\n fileName was incorrect";
 
@@ -153,38 +165,36 @@ int main() {
 	for (int i = 1; i < 15; i++) {
 		value.first = convertType(i).first;
 		value.second = convertType(i).second;
-		std::cout << "\nType " <<i<<" :"<< value.first << "\n";
+		std::cout << "\nType " << i << " :" << value.first << "\n";
 		std::cout << "\nInfo :" << value.second << "\n\n\n";
 
 	}
-	
+
 
 	std::cout << "\nEnter the cursor type to modify:";
 	std::cin >> cursorType;
-	std::cout<<"\n";
-
-	https://www.youtube.com/channel/UCUcexVbcU3qnBY9v8elVCpQ?view_as=subscriber
+	std::cout << "\n";
 
 	DWORD cursorID = convertType(cursorType).first;
 
 	SetSystemCursor(LoadCursor(0, IDC_ARROW), cursorID); // reset cursor
-	
+
 	std::cout << "enter the delay until each cursor picture changes (miliseconds)\n";
-		
+
 	std::cin >> delay;
-	
+
 	std::cout << "\n";
 
 
 		for (int i = 0; i < filesToRead; i++) {
 			Sleep(delay);
 			SetSystemCursor(cursorList[i], cursorID);
-			
-			
+
+
 			//SystemParametersInfoA(SPI_SETCURSORS, 0, NULL, 0); // reload cursors
 			if (i + 1 >= filesToRead) {
 
-				i = (filesToRead == 1) ?-1:0;
+				i = (filesToRead == 1) ? -1 : 0;
 
 
 
@@ -192,7 +202,7 @@ int main() {
 
 					std::string fileNameRevised = (fileName + "\\" + std::to_string(x) + ".CUR");
 
-					cursorList[x-1] = (LoadCursorFromFileA(fileNameRevised.c_str()));
+					cursorList[x - 1] = (LoadCursorFromFileA(fileNameRevised.c_str()));
 
 					if (cursorList[x - 1] == NULL) {
 
@@ -206,10 +216,226 @@ int main() {
 
 
 		}
-		//	std::cout << i;
-			
+			//	std::cout << i;
+
+	}
+
+}
+
+void VideoToCursor() {
+
+	std::cout << "NOTE 1: once you do this operation, you can use them again with method y/Y (you need to look at the largest image name minus 1 to get the number of images). This method also may take more time depending on your computers speed \n\nNOTE; restarting your computer resets any changed cursors. You also need ffmpeg.exe to use this feature of the program; download it from their official website and when extracting the archive it should be in ffmpeg/bin/'ffmpeg' \n\nto use the program(multipule can be open at once) :\n1.fill a folder with the video you want to be rotated through(name must be 'video.mp4' -make sure the folder does not have spaces (and is just lowercase letters)   -- > yes you need a mp4, make a github issue request if someone - for some reason - wants this change)\n2.drag and drop the folder in this window\n3.click enter (sometimes you need to do it twice) \n4.chose which cursor you want to animate(ex: hand arrow) - and number of images to cycle through(putting a impossible value will result in a crash)\n5.the cursor will animate\n";
+
+
+
+	std::cin >> fileNameS;
+	std::cin.sync();
+
+	for (auto i : fileNameS) {
+		if (i != '"') {
+			fileName += i; // this is were files will be saved...
 		}
-		
+	}
 	
-		return 0;
+	std::string VideoFileType = "mp4";
+
+	std::cout << "\n enter your video file type. For example, if your video is video.mp4, write down 'mp4' and click 'enter'   ---> if video.avi, write down 'avi' and click 'enter'";
+
+	std::cin >> VideoFileType;         // --> if someone really cares I'll make a second version that supports more than just whats here, this is bassically all the code I need
+	
+	std::string fileNameExtension = fileName + "\\video."+ VideoFileType; // file and vid
+
+
+	std::string ffmpegLocation;
+	
+	std::cout << "drag and drop the ffmpeg program into this command line and click enter\n\n" << std::endl;
+
+	
+	
+	std::cin>> ffmpegLocation;
+	std::cin.sync();
+
+
+
+	std::string command;
+
+	if (system(ffmpegLocation.c_str()) == 0) { // system outputs cmd commands, so I need to output a command to open and put commands in ffmpeg here
+
+		std::cout << "wrong path to ffmpeg.exe, or the file was renamed";
+
+	}
+
+	std::string fileNameChanged = fileName + "\\%d.jpg"; //lots of pictures... --> you may hit a maximum one day... I think the max is a long long int...
+
+	command = ffmpegLocation +" -r 1 -i "+ fileNameExtension+ " -r 1 "+ fileNameChanged;
+
+	system(command.c_str());
+	
+	std::string nameUnchanged;
+	std::string nameChanged;
+	
+	int count = 0; // number of images
+	
+	
+	while (true) {
+		count += 1; // interate through all items
+
+		fileNameChanged = fileName + "\\" + std::to_string(count)+".jpg";
+
+		nameUnchanged = fileName + "\\" + std::to_string(count) + ".ico";
+
+		nameChanged = fileName + "\\" + std::to_string(count) + ".cur";
+		
+
+		command = ffmpegLocation + " -i " + fileNameChanged + " -vf scale=32:32 "+ nameUnchanged;
+
+		std::ifstream ifile(fileNameChanged.c_str());
+
+		if (!ifile) {
+
+			break;
+
+		}
+
+		ifile.close();
+
+		
+		system(command.c_str());
+		
+		while (true) {//wait until deletes; ffmpeg may not be synced with move file and delete file (its a external cmd process), not allowing delete or renaming (from file use)
+
+			if (DeleteFileA(fileNameChanged.c_str())) {
+
+				break;
+
+			}
+
+		}
+
+		//.cur and .ico are almost always interchangable
+
+		while (true) { // wait until renames; ffmpeg may not be synced with move file and delete file (its a external cmd process), not allowing delete or renaming (from file use)
+		
+			if ((rename((nameUnchanged.c_str()), (nameChanged.c_str())))) {
+
+				break;
+
+			}
+		
+		}
+	}
+
+	count -= 1; // I add one before operations --> number of images
+
+
+	//////////////////////////////////////////
+
+
+
+	std::vector<HCURSOR> cursorList;
+
+	for (int i = 1; i <= count+1; i++) {
+		std::string fileNameRevised = (fileName + "\\" + std::to_string(i) + ".cur");
+
+		cursorList.push_back(LoadCursorFromFileA(fileNameRevised.c_str()));
+
+		if (cursorList[i - 1] == NULL) {
+
+			std::cout << "\n fileName was incorrect";
+
+		}
+
+	}
+
+	for (int i = 0; i < count; i++) { // make sure you have all images
+
+		if (cursorList[i] == NULL) {
+			std::cout << "oof";
+		}
+
+	}
+
+	for (int i = 1; i < 15; i++) {
+		value.first = convertType(i).first;
+		value.second = convertType(i).second;
+		std::cout << "\nType " << i << " :" << value.first << "\n";
+		std::cout << "\nInfo :" << value.second << "\n\n\n";
+
+	}
+
+
+	std::cout << "\nEnter the cursor type to modify:";
+	std::cin >> cursorType;
+	std::cout << "\n";
+
+	DWORD cursorID = convertType(cursorType).first;
+
+	SetSystemCursor(LoadCursor(0, IDC_ARROW), cursorID); // reset cursor
+
+	std::cout << "enter the delay until each cursor picture changes (this value is miliseconds) --> I reccomend making this the fps of the video; math would be INPUT = 1000/fpsOfVideo --> ex: if video is 24fps,  INPUT = 1000/24, INPUT = 42  --> also as a side note; your cursor pointer is pointing at the very left and middle of the square\n";
+
+	std::cin >> delay;
+
+	std::cout << "\n";
+
+
+	for (int i = 0; i < count; i++) {
+		Sleep(delay);
+	
+
+		//SystemParametersInfoA(SPI_SETCURSORS, 0, NULL, 0); // reload cursors
+		if (i + 1 >= count) {
+
+			i = (count == 1) ? -1 : 0;
+
+
+
+			for (int x = 1; x <= count; x++) {
+
+				std::string fileNameRevised = (fileName + "\\" + std::to_string(x) + ".cur");
+
+				cursorList[x - 1] = (LoadCursorFromFileA(fileNameRevised.c_str()));
+
+				if (cursorList[x - 1] == NULL) {
+
+					std::cout << "\n invalid handle made";
+
+				}
+
+			}
+
+
+
+
+		}
+		//	std::cout << i;
+
+	}
+
+
+	///////////////////////////////////////////
+
+
+
+
+}
+
+
+int main() {
+	
+	std::cout << "click 'Y' or 'y' and enter for 'image to cursor', any other letter turns on 'video to cursor'\n\n\n\n\n";
+	std::cin >> check;
+	
+	std::cout << "\n\n\n";
+
+	if (check == 'y' || check == 'Y') {
+		ImageToCursor();
+	}
+	else {
+
+		VideoToCursor();
+
+	}
+
+	return 0;
 }
